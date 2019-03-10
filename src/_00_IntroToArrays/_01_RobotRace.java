fb83281cb529ed30e02bf99c4e88f9851658626b
kcshyffer <league@WorthyMini-6.attlocal.net>
2019-03-10 15:56:36 -0700

    fix the print random string array

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -8,23 +8,48 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
-	
+	public static void main(String[] args) {
+		Random r = new Random();
+		boolean reachedTop = false;
+		Robot topRobot = null;
+		Robot [] robotArray = new Robot [5];
+		for (int i = 0; i < robotArray.length; i++) {
+			robotArray[i] = new Robot();
+			robotArray[i].setX(50+(i*100));
+			robotArray[i].setY(400);
+			robotArray[i].setAngle(0);
+			robotArray[i].setSpeed(20);
+		}
+		while (!reachedTop) {
+			for (int i = 0; i < robotArray.length; i++) {
+				robotArray[i].move(r.nextInt(50));
+			}
+			for (int i = 0; i < robotArray.length; i++) {
+				if (robotArray[i].getY() <= 0) {
+					reachedTop = true;
+					topRobot = robotArray[i];
+				}
+			}
+		}
+		topRobot.turn(360);
+	}
 		//2. create an array of 5 robots.
 
 		//3. use a for loop to initialize the robots.
+
 
 			//4. make each robot start at the bottom of the screen, side by side, facing up
 	
 		//5. use another for loop to iterate through the array and make each robot move 
 	    //   a random amount less than 50.
-    	
+	
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
+		
 		//7. declare that robot the winner and throw it a party!
-    	
+
 		//8. try different races with different amounts of robots.
     	
 	    //9. make the robots race around a circular track.
 
-	
-}
+		}
+
--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,23 +4,63 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
-	
+	public static void main(String[] args) {
+		String [] testArr = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+		printString(testArr);
+		printStringReverse(testArr);
+		printOtherString(testArr);
+		printRandomString(testArr);
+	}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
+	static void printString (String [] s) {
+		for (int i = 0; i < s.length; i++) {
+			System.out.println(s[i]);
+		}
+	}
 	
 	
-	
-	//3. Write a method that takes an array of Strings and prints all the Strings in the array
+	static //3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	
+	void printStringReverse (String [] s) {
+		for (int i = s.length; i < 0; i--) {
+			System.out.println(s[i]);
+		}
+	}
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+	static void printOtherString (String [] s) {
+		for (int i = 0; i < s.length; i++) {
+			if(i%2 == 0) {
+				System.out.println(s[i]);
+			}
+			
+		}
+	}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
-	
-	
+	static void printRandomString (String [] s) {
+		Random r = new Random(s.length);
+		int nextIndex;
+		int [] indexUsed = new int [s.length];
+		for (int i = 0; i < indexUsed.length; i++) {
+			indexUsed[i] = -1;
+		}
+		nextIndex = r.nextInt(s.length);
+		for (int j = 0; j < indexUsed.length; j++) {
+			for (int j2 = 0; j2 < indexUsed.length; j2++) {
+				if(nextIndex == indexUsed[j]) {
+					nextIndex = r.nextInt(s.length);
+					j2 = 0;
+				}
+			}
+			System.out.println(s[nextIndex]);
+		}
+	}
+		
 }
+	
+
